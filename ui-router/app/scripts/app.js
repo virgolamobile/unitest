75779a6038439354df572f74acd95aa2117c5163
unknown <Utente@Utente-PC.(none)>
2014-02-13 19:09:56 +0100

    first

---
+++ ui-router/app/scripts/app.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var app = angular.module('app', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+	$urlRouterProvider.otherwise("/editor");
+
+	// EDITOR
+	$stateProvider
+	.state('editor',
+	{
+		url : "/editor",
+		templateUrl : "views/partials/poster/editor.html"
+	})
+	.state('editor.slide',
+	{
+		url : "/slide",
+		templateUrl : "views/partials/poster/editor/slide.html"
+	})
+	.state('editor.slide.tools',
+	{
+		url : "/tools",
+		templateUrl : "views/partials/poster/editor/slide/tools.html"
+	})
+	.state('editor.slide.tools.text',
+	{
+		url : "/text",
+		templateUrl : "views/partials/poster/editor/slide/tools/text.html",
+		controller : function($scope) {
+			
+		}
+	})
+	.state('editor.slide.tools.font',
+	{
+		url : "/font",
+		templateUrl : "views/partials/poster/editor/slide/tools/font.html",
+		controller : function($scope) {
+			
+		}
+	})
+	.state('editor.slide.tools.color',
+	{
+		url : "/color",
+		templateUrl : "views/partials/poster/editor/slide/tools/color.html",
+		controller : function($scope) {
+			
+		}
+	})
+	.state('editor.slide.tools.image',
+	{
+		url : "/image",
+		templateUrl : "views/partials/poster/editor/slide/tools/image.html",
+		controller : function($scope) {
+			
+		}
+	})
+	.state('editor.slide.tools.wand',
+	{
+		url : "/wand",
+		templateUrl : "views/partials/poster/editor/slide/tools/wand.html",
+		controller : function($scope) {
+			
+		}
+	});
+
+});
