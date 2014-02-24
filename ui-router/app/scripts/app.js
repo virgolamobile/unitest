5f0b2822f026aaf21325a275cb59b3baaf8dafc9
unknown <Utente@Utente-PC.(none)>
2014-02-24 19:07:59 +0100

    init slides, manca il refreshment al cambio di route

--- ui-router/app/scripts/app.js
+++ ui-router/app/scripts/app.js
@@ -5,8 +5,11 @@ var app = angular.module('app', ['ui.router']);
 // ma i servizi non sono che dei facade? mmh...
 app.factory('poster', function() {
 	var services;
+	var activeSlide = {id:0};
+	var slides = [];
 	
-	var slides = {
+	// init slides
+	slides[0] = {
 		text: '',
 		font: '',
 		color: {
@@ -18,38 +21,71 @@ app.factory('poster', function() {
 		wand: {
 			
 		}		
-	}
-	
+	};
+
 	services = {
+
+		// active slide
+		setActiveSlide: function(id)
+		{
+			// segno la slide attiva
+			activeSlide = id;
+
+			// se non esiste la creo
+			if(typeof slides[activeSlide.id] == 'undefined')
+			{
+
+				// creo la nuova slide
+				slides[activeSlide.id] = {
+					text: '',
+					font: '',
+					color: {
+						slideFore: '',
+						slideBack: '',
+						docFore: '',
+						docBack: ''
+					},
+					wand: {
+						
+					}		
+				};
+				
+			}
+			
+		},
+		getActiveSlide: function()
+		{
+			return activeSlide;
+		},
 		
 		// text
 		setText: function(text)
 		{
-			slides.text = text;
+			slides[activeSlide.id].text = text;
 		},
 		getText: function()
 		{
-			return slides.text;
+			return slides[activeSlide.id].text;
 		},
 		
 		// color
 		setColor: function(param,color)
 		{
-			slides.color[param] = color;
+			slides[activeSlide.id].color[param] = color;
 		},
 		getColor: function(param)
 		{
-			return slides.color[param];
+			return slides[activeSlide.id].color[param];
 		},
 		
 		// font
 		setFont: function(font)
 		{
-			slides.font = font;
+			slides[activeSlide.id].font = font;
 		},
 		getFont: function()
 		{
-			return slides.font;
+			return slides[activeSlide.id].font;
 		}
 
 	};
@@ -67,12 +103,38 @@ app.config(function($stateProvider, $urlRouterProvider) {
 	.state('editor',
 	{
 		url : "/editor",
-		templateUrl : "views/partials/poster/editor.html"
+		templateUrl : "views/partials/poster/editor.html",
+		controller: function($scope,poster)
+		{
+			// quante slides ho?
+			$scope.slides = [{id: 0},{id: 1},{id: 2}];
+			
+			// la slide corrente
+			$scope.slide = $scope.slides[1];
+			
+			// il cambio di slide è monitorato
+			$scope.$watch(
+				"slide",
+				function(newvalue,oldvalue)
+				{
+					// prima setto la slide attiva
+					poster.setActiveSlide(newvalue);
+					
+					// poi rigenero lo scope. (ma è corretto? dovrei fare un promise?)
+					$scope.text = poster.text;
+				}
+			);
+
+		}
 	})
 	.state('editor.slide',
 	{
 		url : "/slide",
-		templateUrl : "views/partials/poster/editor/slide.html"
+		templateUrl : "views/partials/poster/editor/slide.html",
+		controller: function($scope)
+		{
+
+		}
 	})
 	.state('editor.slide.tools',
 	{
